b4e232eff1148fe167d5395b609a282581350f7d
iwkt <61336748+iwkt@users.noreply.github.com>
2020-08-03 13:22:27 +0900

    Add files via upload

---
+++ assets/js/abou_imgZoom.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+  function showElementAnimation1() {
+    let element = document.getElementsByClassName('about_pc_img');
+    if (!element) return; // 要素がなかったら処理をキャンセル
+
+    let showTiming = window.innerHeight > 768 ? 260 : 50; // 要素が出てくるタイミングはここで調整
+    let scrollY = window.pageYOffset; //スクロール量を取得
+    let windowH = window.innerHeight; //ブラウザウィンドウのビューポート(viewport)の高さを取得
+
+    for (let i = 0; i < element.length; i++) {
+      let elemClientRect = element[i].getBoundingClientRect();
+      let elemY = scrollY + elemClientRect.top;
+      if (scrollY + windowH - showTiming > elemY) {
+        element[i].classList.add('active');
+      } else if (scrollY + windowH < elemY) {
+        // 上にスクロールして再度非表示にする場合はこちらを記述
+        element[i].classList.remove('active');
+      }
+    }
+  }
+  showElementAnimation1();
+  window.addEventListener('scroll', showElementAnimation1);
+
+});
