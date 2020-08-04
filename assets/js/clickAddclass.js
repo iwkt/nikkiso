dafd1963da1842c58476bce48dfb77351a0625ea
iwkt <iwk@mac.com>
2020-08-04 11:30:21 +0900

    202008041130

--- assets/js/clickAddclass.js
+++ assets/js/clickAddclass.js
@@ -1,7 +1,10 @@
 const parentMenu = document.querySelectorAll('.mobile_menu__main > li > a');
-for(let i = 0; i < parentMenu.length; i++) {
-  parentMenu[i].addEventListener('click',function(e) {
-    e.preventDefault();
-    this.nextElementSibling.classList.toggle('inview');
-  })
-}
+document.addEventListener('DOMContentLoaded', function () {
+  for(let i = 0; i < parentMenu.length; i++) {
+    parentMenu[i].addEventListener('click',function(e) {
+      e.preventDefault();
+      this.classList.toggle('inview');
+      this.nextElementSibling.classList.toggle('inview');
+    })
+  }
+})
