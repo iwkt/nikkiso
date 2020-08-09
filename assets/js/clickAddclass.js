723935a655e685a9e484a04d6049c32b2a28ebb3
iwkt <61336748+iwkt@users.noreply.github.com>
2020-08-09 15:34:53 +0900

    Add files via upload

--- assets/js/clickAddclass.js
+++ assets/js/clickAddclass.js
@@ -40,15 +40,44 @@ document.addEventListener('DOMContentLoaded', function () {
       this.nextElementSibling.classList.toggle('inview');
     })
   }
+});
+
+// const sitemap_acd_parentsMenu = document.querySelectorAll('.site_a');
+// document.addEventListener('DOMContentLoaded', function () {
+//   for (let i = 0; i < sitemap_acd_parentsMenu.length; i++) {
+//     sitemap_acd_parentsMenu[i].addEventListener('click', function (e) {
+//       e.preventDefault();
+//       this.classList.toggle('is-open');
+//       document.getElementById('sitemap_acd').classList.toggle('is-open');
+//     })
+//   }
+// });
+
+const Acd0_ttl = document.querySelector('#Acd0_ttl');
+const Acd1_ul = document.querySelector('#Acd1_ul');
+document.addEventListener('DOMContentLoaded', function (e) {
+    e.preventDefault();
+    Acd0_ttl.addEventListener('click', function () {
+    Acd1_ul.classList.toggle('inview');
+  })
+});
+
+const Acd1_ttl1 = document.querySelector('#Acd1_ttl1');
+const Acd2_ul_1 = document.querySelector('#Acd2_ul_1');
+document.addEventListener('DOMContentLoaded', function () {
+  Acd1_ttl1.addEventListener('click', function (e) {
+    e.preventDefault();
+    Acd2_ul_1.classList.toggle('acd2_1_open');
+    Acd1_ul.classList.toggle('acd2_1_open');    
+  })
 })
 
-const sitemap_acd_parentsMenu = document.querySelectorAll('.site_a');
+const Acd1_ttl2 = document.querySelector('#Acd1_ttl2');
+const Acd2_ul_2 = document.querySelector('#Acd2_ul_2');
 document.addEventListener('DOMContentLoaded', function () {
-  for (let i = 0; i < sitemap_acd_parentsMenu.length; i++) {
-    sitemap_acd_parentsMenu[i].addEventListener('click', function (e) {
-      e.preventDefault();
-      this.classList.toggle('is-open');
-      document.getElementById('sitemap_acd').classList.toggle('is-open');
-    })
-  }
+  Acd1_ttl2.addEventListener('click', function (e) {
+    e.preventDefault();
+    Acd2_ul_2.classList.toggle('acd2_2_open');
+    Acd1_ul.classList.toggle('acd2_2_open');
+  })
 })
