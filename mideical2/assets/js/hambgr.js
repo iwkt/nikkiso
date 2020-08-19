7d73a60ec0593b10d2d4caba12fb57a25d464a40
iwkt <iwk@mac.com>
2020-08-20 06:50:23 +0900

    202008200648

--- mideical2/assets/js/hambgr.js
+++ mideical2/assets/js/hambgr.js
@@ -3,19 +3,40 @@ const container = document.querySelector('#container');
 const cover = document.querySelector('.mm__cover');
 const btn = document.getElementById('navOpen');
 
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('navOpen').addEventListener('click', function () {
-    this.classList.toggle('active');
-    // document.getElementById('MobileMenu').classList.toggle('active');
-    mobilemenu.classList.toggle('active');
-    container.classList.toggle('active');
-    cover.classList.toggle('active');
+function hambgr(
+  ClickBtnName, target_El1, target_El2, target_El3, target_El4,word) {
+  document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector(ClickBtnName).addEventListener('click', function () {
+      this.classList.toggle(word);
+      document.querySelector(target_El1).classList.toggle(word);
+      document.querySelector(target_El2).classList.toggle(word);
+      document.querySelector(target_El3).classList.toggle(word);
+      document.querySelector(target_El4).classList.toggle(word);
+    })
   })
-});
+}
+
+hambgr('#navOpen', '#MM_bk_container', '#container', '.mm__cover','#nav__mm__hdr','active') 
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   document.getElementById('navOpen').addEventListener('click', function () {
+//     this.classList.toggle('active');
+//     // document.getElementById('MobileMenu').classList.toggle('active');
+//     mobilemenu.classList.toggle('active');
+//     container.classList.toggle('active');
+//     cover.classList.toggle('active');
+//   })
+// });
+
+
+// hambgr('.mm__cover', '#MM_bk_container', '#container', '#navOpen', 'active')
+const nav_hdr = document.getElementById('nav__mm__hdr');
 
 cover.addEventListener('click', function () {
   cover.classList.toggle('active');
   mobilemenu.classList.toggle('active');
   container.classList.toggle('active');
   btn.classList.toggle('active');
+  nav_hdr.classList.toggle('active');
+  
 })
